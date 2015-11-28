7071f8a67153a384d0f789e44ef9456209b9e2f5
Alyson Deives <raziel363@gmail.com>
2015-11-27 22:24:32 -0200

    alyson: changed copyright information

--- include/PSkelStencil.hpp
+++ include/PSkelStencil.hpp
@@ -97,7 +97,7 @@ __global__ void stencilTilingCU(Array2D<T1> input,Array2D<T1> output,Mask2D<T2> 
 }
 
 template<typename T1, typename T2, class Args>
-__global__ void stencillTilingCU(Array3D<T1> input,Array3D<T1> output,Mask3D<T2> mask,Args args, size_t widthOffset, size_t heightOffset, size_t depthOffset, size_t tilingWidth, size_t tilingHeight, size_t tilingDepth){
+__global__ void stencilTilingCU(Array3D<T1> input,Array3D<T1> output,Mask3D<T2> mask,Args args, size_t widthOffset, size_t heightOffset, size_t depthOffset, size_t tilingWidth, size_t tilingHeight, size_t tilingDepth){
 	size_t w = blockIdx.x*blockDim.x+threadIdx.x;
 	size_t h = blockIdx.y*blockDim.y+threadIdx.y;
 	size_t d = blockIdx.z*blockDim.z+threadIdx.z;
