bdfdc7cfa626892e1f64834331ee7e32e93c54e5
Alyson Deives Pereira <raziel363@gmail.com>
2016-05-02 20:00:00 -0300

    alyson: debug messages

--- apps/fur/slave.cpp
+++ apps/fur/slave.cpp
@@ -122,6 +122,7 @@ int main(int argc,char **argv) {
   // } else {
   //      stencil.runIterativeMPPA(cluster_id, nb_threads, nb_tiles, iterations);
   //}
+  printf("Processou!\n");
   stencil.~Stencil2D();
-
+  printf("Free!\n");
 }
