68dde272a5e9e30565021a50f4f410a2460d96ef
Alyson Deives <raziel363@gmail.com>
2016-08-18 15:26:21 -0300

    new tests

--- test/run_test_gol.bash
+++ test/run_test_gol.bash
@@ -5,15 +5,17 @@ OUTPUT_FLAG=0
 OPTIMUS=""
 OUTPUT_DIR=""
 EXEC="gol"
-TEST_DIR="./tesla"
-BIN_ACC_KERNELS="../bin/gol_acc_kernels"
-BIN_ACC_PARALLEL="../bin/gol_acc_parallel"
-BIN_PSKEL="../bin/gol_pskel"
+TEST_DIR="./tesla2"
+BIN_ACC_KERNELS="../bin/cloudsim_acc_kernels"
+BIN_ACC_PARALLEL="../bin/cloudsim_acc_parallel"
+BIN_ACC_MULTICORE="../bin/cloudsim_acc_multicore"
+BIN_PSKEL="../bin/cloudsim_pskel"
 ITERATIONS=1
 
 ######TESTES DE TEMPO###########
 make acc_kernels -C ../apps/${EXEC}
 make acc_parallel -C ../apps/${EXEC}
+make acc_multicore -C ../apps/${EXEC}
 make pskel -C ../apps/${EXEC}
 OUTPUT_DIR="${TEST_DIR}/${EXEC}"
 #mkdir ${OUTPUT_DIR}
@@ -24,6 +26,7 @@ GPU_PERCENT=1
 GPU_BLOCK_X=32
 GPU_BLOCK_Y=4
 CPU_THREADS=12
+ITERATIONS=50
 
 for INPUT_SIZE in 512 1024 2048 4096 8192
 do
@@ -32,16 +35,44 @@ do
 		echo $"Running with INPUT_SIZE = ${INPUT_SIZE}"
 		echo "ITERATION #${ITERATION}"
         
-		${BIN_ACC_KERNELS} ${INPUT_SIZE} ${INPUT_SIZE} ${ITERATIONS} ${VERBOSE} &>> ${OUTPUT_DIR}/${EXEC}_kernels_${INPUT_SIZE}_${ITERATIONS}.txt
-		sleep 1
+		#${BIN_ACC_KERNELS} ${INPUT_SIZE} ${INPUT_SIZE} ${ITERATIONS} ${VERBOSE} &>> ${OUTPUT_DIR}/${EXEC}_kernels_${INPUT_SIZE}_${ITERATIONS}.txt
+		#sleep 1
         
         	${BIN_ACC_PARALLEL} ${INPUT_SIZE} ${INPUT_SIZE} ${ITERATIONS} ${VERBOSE} &>> ${OUTPUT_DIR}/${EXEC}_parallel_${INPUT_SIZE}_${ITERATIONS}.txt
 		sleep 1
 
-		${BIN_PSKEL} ${INPUT_SIZE} ${INPUT_SIZE} ${ITERATIONS} ${GPU_PERCENT} ${GPU_BLOCK_X} ${GPU_BLOCK_Y} ${CPU_THREADS} ${VERBOSE} &>> ${OUTPUT_DIR}/${EXEC}_pskel_${INPUT_SIZE}_${ITERATIONS}.txt
+		${BIN_ACC_MULTICORE} ${INPUT_SIZE} ${INPUT_SIZE} ${ITERATIONS} ${VERBOSE} &>> ${OUTPUT_DIR}/${EXEC}_multicore_${INPUT_SIZE}_${ITERATIONS}.txt
 		sleep 1
+
+		${BIN_PSKEL} ${INPUT_SIZE} ${INPUT_SIZE} ${ITERATIONS} ${GPU_PERCENT} ${GPU_BLOCK_X} ${GPU_BLOCK_Y} ${CPU_THREADS} ${VERBOSE} &>> ${OUTPUT_DIR}/${EXEC}_pskelgpu_${INPUT_SIZE}_${ITERATIONS}.txt
+		sleep 1
+		
+		${BIN_PSKEL} ${INPUT_SIZE} ${INPUT_SIZE} ${ITERATIONS} 0 ${GPU_BLOCK_X} ${GPU_BLOCK_Y} ${CPU_THREADS} ${VERBOSE} &>> ${OUTPUT_DIR}/${EXEC}_pskelcpu_${INPUT_SIZE}_${ITERATIONS}.txt
+		sleep 1
+
 	done
 done
+
+#GPU_PERCENT=0
+#INPUT_SIZE=4096
+#ITERATIONS=10
+
+#for CPU_THREADS_SIZE in 1 2 3 4 5 6 7 8 9 10 11 12
+#do
+#        for ITERATION in {1..3..1}
+#        do
+#                echo $"Running with INPUT_SIZE = ${INPUT_SIZE}"
+#                echo "ITERATION #${ITERATION}"
+
+#                OMP_NUM_THREADS=${CPU_THREADS} ${BIN_ACC_MULTICORE} ${INPUT_SIZE} ${INPUT_SIZE} ${ITERATIONS} ${VERBOSE} &>> ${OUTPUT_DIR}/${EXEC}_multicore_${INPUT_SIZE}_${ITERATIONS}_${CPU_THREADS}.txt
+#                sleep 1
+
+#                ${BIN_PSKEL} ${INPUT_SIZE} ${INPUT_SIZE} ${ITERATIONS} ${GPU_PERCENT} ${GPU_BLOCK_X} ${GPU_BLOCK_Y} ${CPU_THREADS} ${VERBOSE} &>> ${OUTPUT_DIR}/${EXEC}_pskelcpu_${INPUT_SIZE}_${ITERATIONS}_${CPU_THREADS}.txt
+#                sleep 1     
+#    
+#        done
+#done
+
 
 #teste com uma variacao de gpu de 0.1 a 0.9 e max threads
 #CPU_THREADS=8
